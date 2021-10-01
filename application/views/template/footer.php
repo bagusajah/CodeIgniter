7b30ac2ed0d32e6e37fe31372dcd371da66e89e7
Bagoes <muhammad.bag@because.consulting>
2021-10-01 17:02:22 +0700

    a

--- application/views/template/footer.php
+++ application/views/template/footer.php
@@ -66,10 +66,8 @@
 
 </footer>
 
-    <script src="assets/js/foundation.js"></script>
-    <script src="assets/js/vendor.js"></script>
     <script src="https://code.jquery.com/jquery-2.1.4.min.js"></script>
-    <script src="assets/js/foundation.js"></script>
+    <script src="https://dhbhdrzi4tiry.cloudfront.net/cdn/sites/foundation.js"></script>
     <script>
       $(document).foundation();
     </script>
--- application/views/template/header.php
+++ application/views/template/header.php
@@ -5,7 +5,7 @@
     <meta http-equiv="x-ua-compatible" content="ie=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Liburan Puas dan Mudah</title>
-    <link rel="stylesheet" href="assets/css/foundation.min.css">
+    <link rel="stylesheet" href="https://dhbhdrzi4tiry.cloudfront.net/cdn/sites/foundation.min.css">
   </head>
 <body>
 
