a2cbbcd9af00c8fb7e887877c0df209752dfb6e2
Bagoes <muhammad.bag@because.consulting>
2021-10-01 14:09:43 +0700

    s

--- application/views/pages/home.php
+++ application/views/pages/home.php
@@ -1,7 +1,3 @@
 <div class="grid-container">
-  <div class="grid-x grid-margin-x">
-    <div class="cell small-4">cell</div>
-    <div class="cell small-4">cell</div>
-    <div class="cell small-4">cell</div>
-  </div>
+  asdfghjkl
 </div>
